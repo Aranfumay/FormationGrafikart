644152c2ccc85646c10ad5dd13e30b49c93918a6
Arnaud LENOIR <arnaud.lenoir@cigest-group.fr>
2020-06-11 15:23:41 +0200

    wfeat: Installation sur le pc professionel et linkage avec phpmyadmin pour la DBO

--- src/Entity/Option.php
+++ src/Entity/Option.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Entity\Property;
 use App\Repository\OptionRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
